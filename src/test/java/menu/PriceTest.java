ab61b31ed03ad4cc3fecd20b63fe96382859d4f9
wooteco-daram <wooteco.daram@gmail.com>
2023-11-10 17:00:56 +0900

    refactor: `PriceTest` 표시명 수정

--- src/test/java/menu/PriceTest.java
+++ src/test/java/menu/PriceTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("가격 객체")
+@DisplayName("가격 테스트")
 class PriceTest {
     @Test
     @DisplayName("가격이 음수이면 IllegalArgumentException 예외를 발생한다.")
