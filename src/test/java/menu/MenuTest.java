f6e8fcf7c87b6393cc0cc86da75ba50bc85ef4fa
wooteco-daram <wooteco.daram@gmail.com>
2023-11-10 16:16:44 +0900

    test: [메뉴] `MenuTest` 에서 메뉴 이름으로 조회하는 `find` 메소드 테스트 케이스 작성

---
+++ src/test/java/menu/MenuTest.java
@@ -0,0 +1,33 @@
+package menu;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import exception.ErrorMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MenuTest {
+    @Test
+    @DisplayName("메뉴 이름으로 메뉴를 찾을 수 있다.")
+    void Should_Get_Menu_When_Find_By_Name() {
+        //given
+        final String name = "양송이수프";
+        final Menu expected = Menu.PINE_MUSHROOM_SOUP;
+
+        //when
+        final Menu actual = Menu.findByName(name);
+
+        //then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("메뉴 이름을 찾을 수 없으면 IllegalArgumentException 예외를 발생시킨다.")
+    void Should_Get_UNKNOWN_When_Name_Not_Found() {
+        //when, then
+        assertThatThrownBy(() -> Menu.findByName("NOT_FOUND"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.INVALID_MENU.getMessage());
+    }
+}
