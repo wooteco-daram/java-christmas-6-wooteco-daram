cf2eb5e877dd1e1d201080954a0008361bca993a
wooteco-daram <wooteco.daram@gmail.com>
2023-11-10 15:11:13 +0900

    feat: [가격] `Price` 레코드 객체 작성 & 음수 검증 로직 추가

---
+++ src/main/java/menu/Price.java
@@ -0,0 +1,14 @@
+package menu;
+
+public record Price(long price) {
+
+    public Price {
+        validateNegativePrice(price);
+    }
+
+    private void validateNegativePrice(final long price) {
+        if (price < 0) {
+            throw new IllegalArgumentException("가격은 음수가 될 수 없습니다.");
+        }
+    }
+}
