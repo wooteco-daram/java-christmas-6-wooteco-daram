735f0d4f1f517926761da825de5d22b12ebc9ffa
wooteco-daram <wooteco.daram@gmail.com>
2023-11-13 18:39:16 +0900

    feat: `EventBadgeType` 에 가격 여부에 따라 조건이 담긴 `pricePredicate` 필드 작성 & 가격으로 조회하는 메소드 추가

--- src/main/java/event/badge/EventBadgeType.java
+++ src/main/java/event/badge/EventBadgeType.java
@@ -1,18 +1,38 @@
 package event.badge;
 
+import java.util.Arrays;
+import java.util.function.Predicate;
+import menu.Price;
+
 public enum EventBadgeType {
-    STAR("별"),
-    TREE("트리"),
-    SANTA("산타"),
-    NONE("없음");
+    NONE("없음", price -> price.price() < 5_000),
+    STAR("별", price -> price.price() >= 5_000),
+    TREE("트리", price -> price.price() >= 10_000),
+    SANTA("산타", price -> price.price() >= 20_000);
 
     private final String name;
+    private final Predicate<Price> pricePredicate;
 
-    EventBadgeType(final String name) {
+    EventBadgeType(
+            final String name,
+            final Predicate<Price> pricePredicate
+    ) {
         this.name = name;
+        this.pricePredicate = pricePredicate;
     }
 
     public String getName() {
         return name;
+    }
+
+    public boolean test(final Price price) {
+        return pricePredicate.test(price);
+    }
+
+    public static EventBadgeType findByPrice(final Price price) {
+        return Arrays.stream(values())
+                .filter(eventBadgeType -> eventBadgeType.test(price))
+                .reduce((first, second) -> second)
+                .orElse(EventBadgeType.NONE);
     }
 }
