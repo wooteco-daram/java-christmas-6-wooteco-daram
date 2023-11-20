d7ed14320fdd542e34dff7fe869ef7ad4732bf6d
wooteco-daram <wooteco.daram@gmail.com>
2023-11-21 01:12:38 +0900

    refactor: `EventConstants` 에서 상수명을 대문자로 변경

--- src/main/java/domain/event/EventConstants.java
+++ src/main/java/domain/event/EventConstants.java
@@ -7,17 +7,17 @@ import java.util.List;
 public class EventConstants {
     public static final int EVENT_YEAR = 2023;
     public static final int EVENT_MONTH = 12;
-    public static final List<DayOfWeek> weekdays = List.of(
+    public static final List<DayOfWeek> WEEKDAYS = List.of(
             DayOfWeek.SUNDAY,
             DayOfWeek.MONDAY,
             DayOfWeek.TUESDAY,
             DayOfWeek.WEDNESDAY,
             DayOfWeek.THURSDAY
     );
-    public static final List<DayOfWeek> weekends = List.of(
+    public static final List<DayOfWeek> WEEKENDS = List.of(
             DayOfWeek.FRIDAY,
             DayOfWeek.SATURDAY
     );
-    public static final List<Integer> starDays = List.of(3, 10, 17, 24, 25, 31);
+    public static final List<Integer> SPECIAL_DAYS = List.of(3, 10, 17, 24, 25, 31);
     public static final Price MINIMUM_TOTAL_PRICE_FOR_DISCOUNT = new Price(10_000L);
 }
