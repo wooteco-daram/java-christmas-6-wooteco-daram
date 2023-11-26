3f4ee50cb37c933ead1a592452c91422640961a9
wooteco-daram <wooteco.daram@gmail.com>
2023-11-26 15:56:31 +0900

    refactor: `DiscountPrice` 에 `from` 생성자 메소드 추가

--- src/main/java/domain/price/DiscountPrice.java
+++ src/main/java/domain/price/DiscountPrice.java
@@ -2,12 +2,16 @@ package domain.price;
 
 public record DiscountPrice(Price price) {
 
+    public static DiscountPrice from(long price) {
+        return new DiscountPrice(new Price(price));
+    }
+
     public static DiscountPrice empty() {
         return new DiscountPrice(Price.empty());
     }
 
     @Override
     public String toString() {
-        return "-" + price.toString();
+        return "-" + price;
     }
 }
