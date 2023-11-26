e0c0d9d502b0f5e912585500d2329b5a28fce8a6
wooteco-daram <wooteco.daram@gmail.com>
2023-11-26 15:56:03 +0900

    refactor: `PaymentPrice` 멤버 변수를 `price` 로 정정

--- src/main/java/domain/price/PaymentPrice.java
+++ src/main/java/domain/price/PaymentPrice.java
@@ -2,19 +2,19 @@ package domain.price;
 
 public class PaymentPrice {
 
-    private final Price paymentPrice;
+    private final Price price;
 
     public PaymentPrice(
             final TotalPrice totalPrice,
             final TotalDiscountPrice totalDiscountPrice
     ) {
-        final Price totalPriceConverted = totalPrice.getTotalPrice();
-        final Price totalDiscountPriceConverted = totalDiscountPrice.getTotalDiscountPrice();
-        paymentPrice = totalPriceConverted.subtract(totalDiscountPriceConverted);
+        final Price totalPriceConverted = totalPrice.getPrice();
+        final Price totalDiscountPriceConverted = totalDiscountPrice.getPrice();
+        price = totalPriceConverted.subtract(totalDiscountPriceConverted);
     }
 
     @Override
     public String toString() {
-        return paymentPrice.toString();
+        return price.toString();
     }
 }
